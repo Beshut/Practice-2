c73c4fee1a5adfb3583f24b59cfa99a88b50e856
Beshut <105577734+Beshut@users.noreply.github.com>
2022-09-30 18:16:58 +0300

    Create Program1.cs

---
+++ Program1.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program1
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("5\n10\n21");//вывод на экран чисел в столбик
+        Console.ReadKey();//ожидание нажатия клавиши
+    }
+}
